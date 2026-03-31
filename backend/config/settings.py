03d8523d3af1501a2393f7ca6990270c2b0d33ef
Dennis Ngetich <dennischeruiyotngetich@gmail.com>
2026-03-31 15:43:50 +0300

    Configure DATABASE_URL for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,21 +76,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
-if dj_database_url:
+if dj_database_url and os.getenv('DATABASE_URL'):
     DATABASES = {
         'default': dj_database_url.config(
             default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
             conn_max_age=600,
+            conn_health_checks=True,
         )
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    
 
+    
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
